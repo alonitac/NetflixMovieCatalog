05b63eb85dbfce3130a4bb0a2dff3b2509b08866
YaelWil <yaelwilamo@gmail.com>
2024-08-01 16:05:15 +0300

    commit 01.08.2024 16:00

--- app.py
+++ app.py
@@ -61,6 +61,7 @@ def update_popularity():
 
     return jsonify({'message': 'Popularity updated successfully', 'new_popularity': new_popularity}), 200
 
+
 @app.route('/status')
 def status():
     return 'OK'
